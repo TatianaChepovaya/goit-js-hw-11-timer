036e11710ed050dde902397a38cf1d81f6c11eed
TatianaChepovaya <taniacheport@gmail.com>
2021-09-05 11:50:19 +0300

    правка

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 import CountdownTimer from './timer.js';
 
-const  timer = new CountdownTimer({
+const timer = new CountdownTimer({
   selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
+  targetDate: new Date('Sep 30, 2022'),
 });
-
-
-
--- js/timer.js
+++ js/timer.js
@@ -1,20 +1,18 @@
-
-
 export default class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.selector = selector;
     this.targetDate = targetDate;
     this.intervalId = null;
     this.refs = {
-  days: document.querySelector(`${this.selector} [data-value="days"]`),
-  hours: document.querySelector(`${this.selector} [data-value="hours"]`),
-  mins: document.querySelector(`${this.selector} [data-value="mins"]`),
-  secs: document.querySelector(`${this.selector} [data-value="secs"]`),
-}
+      days: document.querySelector(`${this.selector} [data-value="days"]`),
+      hours: document.querySelector(`${this.selector} [data-value="hours"]`),
+      mins: document.querySelector(`${this.selector} [data-value="mins"]`),
+      secs: document.querySelector(`${this.selector} [data-value="secs"]`),
+    };
 
     this.start();
   }
-  
+
   start() {
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
@@ -22,26 +20,27 @@ export default class CountdownTimer {
       const { days, hours, mins, secs } = this.getTimeComponents(time);
       this.refs.days.textContent = days;
       this.refs.hours.textContent = hours;
-      this.refs.mins.textContent= mins;
-      this.refs.secs.textContent= secs;
+      this.refs.mins.textContent = mins;
+      this.refs.secs.textContent = secs;
 
       if (time <= 0) {
-        clearInterval(intervalId);
+        this.clearInterval(intervalId);
         this.getTimeComponents(0);
       }
     }, 1000);
   }
 
   getTimeComponents(time) {
-      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-      const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
-      return { days, hours, mins, secs };
+    return { days, hours, mins, secs };
   }
   pad(value) {
-        return String(value).padStart(2, '0');
-      }
-  
+    return String(value).padStart(2, '0');
+  }
 }
